131b34132b21cb61b591b8242f4ab102ec52ee12
Klare Frank <klarefrank@Klares-MacBook-Pro.local>
2018-11-30 12:21:27 -0800

    commit via processing

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gitbit
 A project for HCDE 539, Physical Computing, at the University of Washington
+
+commit
---
+++ test.sh
@@ -0,0 +1,4 @@
+echo "commit" >> /Users/klarefrank/Dev/gitbit/README.md
+git add -A && \
+git commit -m "new commit via processing" && \
+git push https://github.com/kfrank/gitbit.git master
