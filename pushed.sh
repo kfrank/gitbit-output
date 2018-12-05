89e17875b7e2baf911e0de1d1684ca4633d8f13b
Klare Frank <klarefrank@D-10-18-249-27.dhcp4.washington.edu>
2018-12-04 17:35:03 -0800

    commit via gitbit device

--- pushed.sh
+++ pushed.sh
@@ -1,5 +1,5 @@
 cd /Users/klarefrank/Dev/gitbit-output
 echo "commit" >> README.md
 git add -A
-git commit -m "commit via processing"
+git commit -m "commit via gitbit device"
 git push git@github.com:kfrank/gitbit-output.git master
